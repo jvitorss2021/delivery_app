dd38ebf51aacc95bfaceba43078d8febf0cacaa4
Joao Vitor <jvitorss2021@gmail.com>
2024-11-22 14:00:48 -0300

    fix: tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 const Home: React.FC = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black-100">
+      <h1 className="text-3xl font-bold text-blue-500 mb-4">
         Bem-vindo ao Delivery App 🚀
       </h1>
-      <h1>O melhor lugar para pedir sua comida favorita!</h1>
-      <div className="flex space-x-4">
+      <h2 className="text-xl mb-8">
+        O melhor lugar para pedir sua comida favorita!
+      </h2>
+      <div className="flex flex-row space-x-4">
         <a
           href="/products"
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 text-white py-1.5 px-2.5 rounded hover:bg-blue-600"
         >
           Produtos
         </a>
         <a
           href="/carrinho"
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 text-white py-1.5 px-2.5 rounded hover:bg-blue-600"
         >
           Carrinho
         </a>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,15 +4,18 @@ import ProductCard from "../components/ProductCard";
 
 const Products: React.FC = () => {
   const products = [
-    { id: 1, name: "Pizza", price: "R$ 25,00", image: "pizza.jpg" },
-    { id: 2, name: "Hambúrguer", price: "R$ 20,00", image: "burger.jpg" },
-    { id: 3, name: "Sushi", price: "R$ 40,00", image: "sushi.jpg" },
+    { id: 1, name: "Hambúrguer", price: "R$ 20,00", image: "burger.jpg" },
+    { id: 2, name: "Batata Frita", price: "R$ 12,00", image: "fries.jpg" },
+    { id: 3, name: "Pizza", price: "R$ 25,00", image: "pizza.jpg" },
+    { id: 4, name: "Sushi", price: "R$ 40,00", image: "sushi.jpg" },
+    { id: 5, name: "poke", price: "R$ 35,00", image: "poke.jpg" },
+    { id: 6, name: "Salada", price: "R$ 15,00", image: "salad.jpg" },
   ];
 
   return (
     <>
       <Header />
-      <main className="container mx-auto py-8">
+      <main className="container mx-auto py-8 mb-8">
         <h1 className="text-2xl font-bold mb-4">Produtos</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {products.map((product) => (
