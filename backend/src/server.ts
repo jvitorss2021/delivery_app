5a4b0cc140858e77e8f02478686600a840ca6aa6
Joao Vitor <jvitorss2021@gmail.com>
2024-11-29 12:52:40 -0300

    teste

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,8 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 // Rotas
-app.use("/api/products", productRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
